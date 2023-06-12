30d8482b450636af55da554aaf59e7dcdfa75047
peter <34286598+peter6C6F6C@users.noreply.github.com>
2023-06-12 04:03:35 -0400

    Create config.py

---
+++ python/config.py
@@ -0,0 +1,6 @@
+mysql_user = "your_mysql_user"
+mysql_password = "your_mysql_pass"
+mysql_host = "your_mysql_host"
+mysql_port = 3306
+mysql_database = "your_mysql_database"
+admin_password = "imagetest" # used to access the admin panel via the web UI.
